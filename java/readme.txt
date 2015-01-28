5a0f415a468ecf471c44805578186337e66568b9
chaohan <alexhan@host153-121.vpn.ubc.ca>
2015-01-28 10:30:34 -0800

    reorg

--- java/readme.txt
+++
@@ -1,5 +0,0 @@
-A programming exercise for
-
-http://coursera.cs.princeton.edu/algs4/assignments/kdtree.html
-
-Classes KdTree and Node are mine.
--- resume-html/js/time.js
+++
@@ -1,6 +0,0 @@
-var today = new Date();
-var hours = today.getHours();
-var minutes = today.getMinutes();
-if(minutes<10){minutes ='0'+minutes};
-var element = document.getElementById('time');
-element.textContent = 'Current time: ' + hours + ':' + minutes;
